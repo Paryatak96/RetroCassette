96ca82434f5b9a7e0adb1168fe0df9531503b9c4
Sebastian Liszewski <Sebastian.Liszewski@outlook.com>
2024-04-11 19:01:01 +0200

    Add new projects: Application, Domain, Infrastructure, and some classes with models.

---
+++ RetoroCassette.Domain/Model/Cassette.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RetoroCassette.Domain.Model
+{
+    public class Cassette
+    {
+        public int Id { get; set; }
+        public string MovieTitle { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public int MovieLength { get; set; }
+        public Director DirectorOfMovie { get; set; }
+        public Genre GenreOfMovie { get; set; }
+        public bool Availability { get; set; }
+        public string Description { get; set; }
+        public string Language { get; set; }
+    }
+}
